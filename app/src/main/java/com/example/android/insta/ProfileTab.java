e3d6a1762e25971f135695e8d6a46c36d73868fd
Prakhar <000.excel@gmail.com>
2019-07-25 11:15:29 +0530

    profile tab Implemented
    Signed-off-by: Prakhar <000.excel@gmail.com>

--- app/src/main/java/com/example/android/insta/ProfileTab.java
+++ app/src/main/java/com/example/android/insta/ProfileTab.java
@@ -2,11 +2,19 @@ package com.example.android.insta;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 
 /**
@@ -14,6 +22,8 @@ import androidx.fragment.app.Fragment;
  */
 public class ProfileTab extends Fragment {
 
+    EditText name, bio, profession, hobbies, sport;
+    Button updateInfo;
 
     public ProfileTab() {
         // Required empty public constructor
@@ -24,7 +34,48 @@ public class ProfileTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        name = view.findViewById(R.id.name);
+        bio = view.findViewById(R.id.bio);
+        profession = view.findViewById(R.id.profession);
+        hobbies = view.findViewById(R.id.hobbie);
+        sport = view.findViewById(R.id.sport);
+        updateInfo = view.findViewById(R.id.updateProfile);
+
+        final ParseUser parseUser = ParseUser.getCurrentUser();
+
+        try {
+            //get data
+            name.setText(parseUser.get("ProfileName").toString());
+            bio.setText(parseUser.get("Bio").toString());
+            profession.setText(parseUser.get("Profession").toString());
+            hobbies.setText(parseUser.get("Hobbie").toString());
+            sport.setText(parseUser.get("Sport").toString());
+        } catch (NullPointerException e) {
+            Log.i("profileTab", e + "");
+        }
+        updateInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                parseUser.put("ProfileName", name.getText().toString());
+                parseUser.put("Bio", bio.getText().toString());
+                parseUser.put("Profession", profession.getText().toString());
+                parseUser.put("Sport", sport.getText().toString());
+                parseUser.put("Hobbie", hobbies.getText().toString());
+                parseUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Toast.makeText(getContext(), "Info Updated", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(getContext(), "Failed " + e, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+            }
+        });
+        return view;
     }
 
 }
