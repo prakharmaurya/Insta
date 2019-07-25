a77c276f87c47ef3926d0a6f5748ab37474b5590
Prakhar <000.excel@gmail.com>
2019-07-25 08:29:32 +0530

    Bug Fixes Button text changes on main and background thread new Social layout added
    Signed-off-by: Prakhar <000.excel@gmail.com>

--- app/src/main/java/com/example/android/insta/LogIn.java
+++ app/src/main/java/com/example/android/insta/LogIn.java
@@ -2,7 +2,9 @@ package com.example.android.insta;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -17,16 +19,37 @@ import com.parse.ParseUser;
 public class LogIn extends AppCompatActivity implements View.OnClickListener {
     EditText userName, password;
     Button signIn, signUp;
-
     @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+        findViewById(R.id.rootSignIn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+            }
+        });
         signUp = findViewById(R.id.logSignUp);
         signIn = findViewById(R.id.logSignIn);
         userName = findViewById(R.id.loguserName);
         password = findViewById(R.id.logPassword);
+        password.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    onClick(signIn);
+                }
+                return false;
+            }
+        });
+
+        if (ParseUser.getCurrentUser() != null) {
+            ParseUser.getCurrentUser().logOut();
+//            Intent i = new Intent(getApplicationContext(), SocialMediaActivity.class);
+//            startActivity(i);
+        }
 
         signIn.setOnClickListener(LogIn.this);
         signUp.setOnClickListener(LogIn.this);
@@ -38,15 +61,33 @@ public class LogIn extends AppCompatActivity implements View.OnClickListener {
             Intent i = new Intent(getApplicationContext(), SignUp.class);
             startActivity(i);
         } else if (v == signIn) {
-            ParseUser.logInInBackground(userName.getText().toString(), password.getText().toString(), new LogInCallback() {
-                public void done(ParseUser user, ParseException e) {
-                    if (user != null) {
-                        Toast.makeText(getApplicationContext(), "LoggedIn", Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(getApplicationContext(), "" + e, Toast.LENGTH_LONG).show();
+            if (userName.getText().toString().equals("")
+                    || password.getText().toString().equals("")) {
+                Toast.makeText(getApplicationContext(), "Fill All", Toast.LENGTH_LONG).show();
+            } else {
+                signIn.setText("Please Wait");
+                signIn.setEnabled(false);
+                signIn.setAlpha(0.5f);
+                ParseUser.logInInBackground(userName.getText().toString(), password.getText().toString(), new LogInCallback() {
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null && e == null) {
+                            //Toast.makeText(getApplicationContext(), "LoggedIn", Toast.LENGTH_LONG).show();
+                            Intent i = new Intent(getApplicationContext(), SocialMediaActivity.class);
+                            startActivity(i);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    signIn.setText("SIGN IN");
+                                    signIn.setAlpha(1f);
+                                    signIn.setEnabled(true);
+                                }
+                            });
+                        } else {
+                            Toast.makeText(getApplicationContext(), "" + e, Toast.LENGTH_LONG).show();
+                        }
                     }
-                }
-            });
+                });
+            }
         }
     }
 }
--- app/src/main/java/com/example/android/insta/SignUp.java
+++ app/src/main/java/com/example/android/insta/SignUp.java
@@ -2,7 +2,9 @@ package com.example.android.insta;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -22,13 +24,29 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        androidx.appcompat.app.ActionBar ab = getSupportActionBar();
-        ab.setTitle("Sign Up");
+        setTitle("Sign Up");
+
+        findViewById(R.id.root_sign_up).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+            }
+        });
 
         signUp = findViewById(R.id.signUp);
         signIn = findViewById(R.id.signIn);
         userName = findViewById(R.id.userName);
         password = findViewById(R.id.password);
+        password.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    onClick(signUp);
+                }
+                return false;
+            }
+        });
         email = findViewById(R.id.email);
 
         signIn.setOnClickListener(this);
@@ -38,19 +56,37 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         if (v == signUp) {
-            ParseUser user = new ParseUser();
-            user.setUsername(userName.getText().toString());
-            user.setPassword(password.getText().toString());
-            user.setEmail(email.getText().toString());
-            user.signUpInBackground(new SignUpCallback() {
-                public void done(ParseException e) {
-                    if (e == null) {
-                        Toast.makeText(getApplicationContext(), "Loggedin", Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(getApplicationContext(), "" + e, Toast.LENGTH_LONG).show();
+            if (email.getText().toString().equals("") || userName.getText().toString().equals("")
+                    || password.getText().toString().equals("")) {
+                Toast.makeText(getApplicationContext(), "Fill all info", Toast.LENGTH_LONG).show();
+            } else {
+                ParseUser user = new ParseUser();
+                user.setUsername(userName.getText().toString());
+                user.setPassword(password.getText().toString());
+                user.setEmail(email.getText().toString());
+                signUp.setText("Please Wait");
+                signUp.setAlpha(0.5f);
+                signUp.setEnabled(false);
+                user.signUpInBackground(new SignUpCallback() {
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            //Toast.makeText(getApplicationContext(), "Loggedin", Toast.LENGTH_LONG).show();
+                            Intent i = new Intent(getApplicationContext(), SocialMediaActivity.class);
+                            startActivity(i);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    signUp.setText("SIGN IN");
+                                    signUp.setAlpha(1f);
+                                    signUp.setEnabled(true);
+                                }
+                            });
+                        } else {
+                            Toast.makeText(getApplicationContext(), "" + e, Toast.LENGTH_LONG).show();
+                        }
                     }
-                }
-            });
+                });
+            }
         } else if (v == signIn) {
             Toast.makeText(getApplicationContext(), "signIn", Toast.LENGTH_LONG).show();
             Intent i = new Intent(getApplicationContext(), LogIn.class);
