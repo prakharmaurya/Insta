7ba2808ab765c3af14dd9ffe4f4b7b1f67cb32fd
Prakhar <000.excel@gmail.com>
2019-07-25 10:35:38 +0530

    new Social layout added tab added and Implemented
    Signed-off-by: Prakhar <000.excel@gmail.com>

--- app/src/main/java/com/example/android/insta/SocialMediaActivity.java
+++ app/src/main/java/com/example/android/insta/SocialMediaActivity.java
@@ -4,12 +4,33 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class SocialMediaActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    ViewPager viewPager;
+    TabLayout tabLayout;
+    TabAdapter tabAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.myToobar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+        setTitle("Social Media");
+
+        viewPager = findViewById(R.id.view_pager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager, false);
     }
 }
