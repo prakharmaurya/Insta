34f375c50f444284d10a9d684265058bb9085725
Prakhar <000.excel@gmail.com>
2019-07-25 11:35:38 +0530

    user tab listView added without using default layout
    Signed-off-by: Prakhar <000.excel@gmail.com>

--- app/src/main/java/com/example/android/insta/UsersTab.java
+++ app/src/main/java/com/example/android/insta/UsersTab.java
@@ -5,8 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -14,6 +24,10 @@ import androidx.fragment.app.Fragment;
  */
 public class UsersTab extends Fragment {
 
+    ListView listView;
+
+    ArrayList arrayList;
+    ArrayAdapter arrayAdapter;
 
     public UsersTab() {
         // Required empty public constructor
@@ -24,7 +38,30 @@ public class UsersTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_users_tab, container, false);
+        listView = view.findViewById(R.id.listView);
+        arrayList = new ArrayList();
+        arrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, arrayList);
+
+        //Importing Users
+        final ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        parseQuery.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
+
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+                if (e == null) {
+                    if (users.size() > 0) {
+                        for (ParseUser user : users) {
+                            arrayList.add(user.getUsername());
+                        }
+                        listView.setAdapter(arrayAdapter);
+                    }
+                }
+            }
+        });
+
+        return view;
     }
 
 }
